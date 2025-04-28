3bbdecf0fbf4ad2bafd8c2abe87369d0b2b70296
Art <simf.i@ya.ru>
2025-04-28 23:55:30 +0300

    aboutus responsive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import { Footer } from 'widgets/footer/Footer';
 import { BrowserRouter } from 'react-router-dom';
 import { Header } from 'widgets/header';
 import { MainContent } from 'features/mainContent';
-import { ContactForm } from 'shared/сontactForm';
 import './styles/global.scss';
 
 export const App: FC = () => {
