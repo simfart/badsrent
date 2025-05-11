a93b1504d280d2047df41e7177eb25641d23391a
Art <simf.i@ya.ru>
2025-05-11 23:07:23 +0300

    MainDescription component

--- src/features/mainContent/MainContent.tsx
+++ src/features/mainContent/MainContent.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { AboutUs } from 'features/aboutUs/AboutUs';
 import { ProductList } from 'features/productList/ProductList';
 import styles from './MainContent.module.scss';
+import { MainDescription } from './mainDescription/mainDescription';
 
 export const MainContent: FC = () => {
   return (
@@ -20,27 +21,7 @@ export const MainContent: FC = () => {
           <li>Оплата после получения кровати</li>
         </ul>
       </section>
-      <div className={styles.mainDescr} itemProp="description">
-        Мы предоставляем в аренду медицинские кровати для лежачих больных с
-        доставкой по всему Крыму: Симферополь, Ялта, Севастополь, Евпатория и
-        другие города. У нас вы можете{' '}
-        <strong>взять в аренду медицинскую кровать</strong> с электроприводом,
-        функцией переворота и трансформацией в кардио-кресло.
-        <br />
-        <br />
-        Наша компания предлагает{' '}
-        <strong>функциональные кровати для лежачих больных в аренду</strong>,
-        включая модели с туалетным устройством и штангой для подтягивания. Мы
-        знаем, насколько важен комфорт и безопасность при уходе за близкими,
-        поэтому тщательно подбираем каждую модель оборудования.
-        <br />
-        <br />
-        Услуга <strong>аренды медицинской кровати</strong> подходит как для
-        краткосрочного, так и для длительного ухода на дому. Независимо от того,
-        нужна ли вам <strong>медицинская кровать с электроприводом</strong> или
-        простая механическая модель, мы подберём оптимальное решение и доставим
-        кровать в кратчайшие сроки.
-      </div>
+      <MainDescription />
       <AboutUs />
 
       <ProductList />
